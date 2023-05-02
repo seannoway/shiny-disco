7557474dea3925a60c8e70b46794c6c7a1f2a5cf
Sean Noway <sean.m.holian@gmail.com>
2023-05-02 18:13:20 -0400

    updated links to get everything rendering

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = moment().format("dddd, MMMM Do YYYY");
+  
+  $("#currentDay").text(currentDate);
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
